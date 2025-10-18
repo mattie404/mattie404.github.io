983688b5b0eb44cd96d71e3f935a64c7c726d7ba
Gocsál Mátyás <145768219+gocsaldev@users.noreply.github.com>
2025-10-19 01:20:03 +0200

    New BG image, navbar is ready to use, added footer and 88x31 buttons + copyright

--- scripts/lang.js
+++ scripts/lang.js
@@ -1,7 +1,12 @@
 let languageSelect = document.getElementById("languageSelect"),
     title = document.querySelector(".title"),
-    descr = document.querySelector(".titleDesc");
-    langLab = document.querySelector(".langLab");
+    descr = document.querySelector(".titleDesc"),
+    langLab = document.querySelector(".langLab"),
+    home = document.querySelector(".home a");
+    aboutme = document.querySelector(".aboutme a");
+    guest = document.querySelector(".guestbook a");
+    projects = document.querySelector(".projects a");
+    contact = document.querySelector(".contact a");
 
 languageSelect.addEventListener("change", () => {
     let selectedLang = languageSelect.value;
@@ -9,18 +14,33 @@ languageSelect.addEventListener("change", () => {
     title.textContent = data[selectedLang].title;
     descr.textContent = data[selectedLang].titleDesc;
     langLab.textContent = data[selectedLang].langLab;
+    home.textContent = data[selectedLang].home;
+    aboutme.textContent = data[selectedLang].aboutme;
+    guest.textContent = data[selectedLang].guest;
+    projects.textContent = data[selectedLang].projects;
+    contact.textContent = data[selectedLang].contact;
 });
 
 let data = {
     english: {
         "title": "Matt's Website",
         "titleDesc": "Welcome to my personal website!",
-        "langLab": "Select Language: "
+        "langLab": "Select Language: ",
+        "home": "Home",
+        "aboutme": "About Me",
+        "guest": "Guestbook",
+        "projects": "Projects",
+        "contact": "Contact",
     },
 
     hungarian: {
-        "title": "Matt's Website",
+        "title": "Matyi Weboldala",
         "titleDesc": "Üdvözöllek a személyes weboldalamon!",
-        "langLab": "Válassz nyelvet: "
+        "langLab": "Válassz nyelvet: ",
+        "home": "Honlap",
+        "aboutme": "Rólam",
+        "guest": "Vendégkönyv",
+        "projects": "Projektek",
+        "contact": "Elérhetőség",
     }
 }
